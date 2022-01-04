8c3c31467212035c733e5e00370c26d74162b609
Petzr <peterjansen812@gmail.com>
2022-01-04 20:04:23 +0100

    added: show board changed create board

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,34 @@ public class Main {
 
     public static void main(String[] args) {
 	    // creating board
-        String[] board = createBoard(4);
+        String[][] board = createBoard(4);
+
+        printBoard(board);
     }
 
-    public static String[] createBoard(int numb) {
-        String[] board = new String[numb];
+    public static String[][] createBoard(int numb) {
 
-        for (int i=0; i < board.length; i++) {
-            board[i] = ".";
+        String[][] board = new String[numb][numb];
+
+        for (int i=0; i < board.length; i++) {                  // rows
+            for (int j=0; j<board[0].length; j++) {             // columns
+                board[i][j] = ".";
+            }
         }
 
         return board;
+    }
+
+    public static void printBoard(String[][] board) {
+
+        for (int i=0; i < board.length; i++) {
+            for (int j=0; j<board[0].length; j++) {
+                if (j != board[0].length-1) {
+                    System.out.print(board[i][j] +" | ");
+                } else {
+                    System.out.println(board[i][j]);
+                }
+            }
+        }
     }
 }
